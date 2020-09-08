d6c6f07b794a0f3e83b3b52335841cf099b6b193
candtechsoftware <alex@candtechsoftware.com>
2020-09-08 17:00:52 -0400

    added some config files

---
+++ src/components/menu-item/menu-item.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+
+const MenuItem = () => (
+                <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <span className="title">Shop Now</span>
+                </div>
+            </div>
+
+)
---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Homepage.scss';
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+ 
+        </div>
+    </div>
+)
+
+export default HomePage
